ceed374a84e1106d1b6892462567c87f84ee8136
kindy <kinti462@gmail.com>
2019-09-16 20:50:32 +0300

    Добавьте файлы проекта.

---
+++ Figure/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FigureLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialX;
+            double finalX;
+            int menu;
+            CurvedTrapezoid curvedTrapezoid = new CurvedTrapezoid(0,0);
+            do
+            {
+                Console.WriteLine("*************MENU*************");
+                Console.WriteLine("1. Create a curved trapezoid");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("*****************************");
+                Console.Write("Enter menu item: ");
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        Console.Write("Enter coordinate initial X: ");
+                        initialX = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter coordinate final X: ");
+                        finalX = Convert.ToDouble(Console.ReadLine());
+                        curvedTrapezoid.ExistenceCheck(initialX, finalX);
+                        break;
+                }
+            } while (menu != 2);
+        }
+    }
+}
---
+++ FigureLibrary/CurvedTrapezoid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLibrary
+{
+    public class CurvedTrapezoid
+    {
+        double XInitial { get; set; }
+        double XFinal { get; set; }
+        public CurvedTrapezoid(double XInitial, double XFinal)
+        {
+            this.XInitial = XInitial;
+            this.XFinal = XFinal;
+        }
+        public void ExistenceCheck(double XInitial, double XFinal)
+        {
+            if (((XInitial < 0 ) && (XFinal > 0)) || ((XInitial > 0) && (XFinal < 0)) || ((XInitial == 0) && (XFinal == 0)))
+            {
+                Console.WriteLine("A figure cannot exist.");
+            }
+            else
+            {
+                Console.WriteLine("Data entered is coorect.");
+            }
+        }
+
+    }
+}
